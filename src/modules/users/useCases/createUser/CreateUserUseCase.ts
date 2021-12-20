a974e25bb7e3f738c9a7a3d4fad229d3f8b2b6be
Luís Miguel Silva Amorim <luismiguelsilvaamorim@gmail.com>
2021-12-20 16:12:57 -0300

    finish challenge

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const findUser = this.usersRepository.findByEmail(email);
+
+    if(findUser){
+      throw new Error('Email already in use');
+    };
+
+    const user = this.usersRepository.create({name, email});
+
+    return user;
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.body;
+
+    try{
+      const admin = this.turnUserAdminUseCase.execute({user_id});
+
+      return response.status(200).json({admin});
+    }catch(err){
+      return response.status(404).json({error: err});
+    }
   }
 }
 
